340171097e3959cac61144ef9574cbc7520b6627
Jordan Wood <up826858@myport.ac.uk>
2020-02-18 21:10:08 +0000

    Fix audio

--- Assets/Scripts/InGame/BezierPlayer.cs
+++ Assets/Scripts/InGame/BezierPlayer.cs
@@ -11,29 +11,32 @@ public class BezierPlayer : MonoBehaviour {
     public Vector2 offset;
     public GameObject playerObject;
     public GameObject trackerObject;
-    public AudioController audioSource;
 
     [Header("Track Settings")]
     public Vector3 trackerStartPos;
     public Vector3 trackerPosDiff;
+    public AudioSource AudioSource;
 
     [Header("User Interface")]
     public TextMeshProUGUI ScoreUI;
     public TextMeshProUGUI ComboUI;
     public TextMeshProUGUI MultiplierUI;
-
-    private BGCcMath curve;
-    private float time;
-    private float distance;
-    private float duration = 10;
-
+    public TextMeshProUGUI MissesUI;
+    
     [Header("Debug")]
     public int Score = 0;
     public int Streak = 0;
     public int Multiplier = 1;
     public int Misses = 0;
     
+    private BGCcMath curve;
+    private float time;
+    private float distance;
+    private float duration = 10;
+    
     private bool playing = false;
+    private Config config;
+    private bool started = false;
 
     private void OnCollisionEnter(Collision other) {
         throw new NotImplementedException();
@@ -44,14 +47,15 @@ public class BezierPlayer : MonoBehaviour {
         
         StaticEvents.NoteHitEvent.AddListener(OnNoteHit);
         StaticEvents.NoteMissEvent.AddListener(OnNoteMiss);
+        
+        config = configLoader.config;
     }
 
     private Vector3 lastPoint;
     private bool set = false;
     private void Update() {
-        Config config = configLoader.config;
         if (config != null) duration = (config.duration.minutes * 60) + config.duration.seconds;
-        
+
         if (time < 2 && !set) {
             time += Time.deltaTime;
             return;
@@ -61,10 +65,6 @@ public class BezierPlayer : MonoBehaviour {
             trackerStartPos = trackerObject.transform.localPosition;
             set = true;
         }
-
-        ScoreUI.text = "Score: " + Score;
-        ComboUI.text = "Combo: " + Streak;
-        MultiplierUI.text = "Multi: " + Multiplier;
 
         trackerPosDiff = trackerStartPos - trackerObject.transform.localPosition;
         offset += new Vector2((-trackerPosDiff.x) / 100, 0);
@@ -77,18 +77,35 @@ public class BezierPlayer : MonoBehaviour {
         transform.rotation = Quaternion.LookRotation(tangent);
 
         playerObject.transform.localPosition = new Vector3(offset.x, offset.y + 0.562f);
+
+        if (started == false) {
+            var delay = (1f / config.beatsPerMinute) * 5f;
+            AudioSource.PlayDelayed(delay);
+            started = true;
+        }
     }
 
     void OnNoteHit() {
         Multiplier = Mathf.FloorToInt(f: Streak / 15f) + 1;
-        if (Multiplier >= 4) Multiplier = 4;
+        if (Multiplier > 4) Multiplier = 4;
 
         Score += Multiplier;
         Streak++;
+
+        UpdateUI();
     }
 
     void OnNoteMiss() {
         Streak = 0;
         Misses++;
+        
+        UpdateUI();
+    }
+
+    void UpdateUI() {
+        ScoreUI.text = "Score: " + Score;
+        ComboUI.text = "Combo: " + Streak;
+        MultiplierUI.text = "Multi: " + Multiplier;
+        MissesUI.text = "Misses: " + Misses;
     }
 }
